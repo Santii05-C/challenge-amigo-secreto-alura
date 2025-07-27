ecc4d03ef5781a75b03d566199e220931b69437e
Santii05-C <santi.c.pgm@gmail.com>
2025-07-27 20:58:36 -0300

    Creación de la función de la lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function mostrarAmigos(amigos) {
-  // 1. Obtener el elemento de la lista
+  // Obtener el elemento de la lista de amigos
   const lista = document.getElementById("listaAmigos");
 
-  // 2. Limpiar la lista existente
+  //  Limpiar la lista existente
   lista.innerHTML = "";
 
-  // 3. Iterar sobre el arreglo y 4. Agregar elementos a la lista
+  // Iterar sobre el arreglo y 4. Agregar elementos a la lista
   for (let i = 0; i < amigos.length; i++) {
     lista.innerHTML += `<li>${amigos[i]}</li>`;
   }
 }
-const misAmigos = ["Juan", "María", "Carlos", "Ana"];
+const misAmigos = ["Sergio", "Nicolas", "Agustina", "Estela"];
 mostrarAmigos(misAmigos);
