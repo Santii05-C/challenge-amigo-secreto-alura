3c871c83e00c61e2ac996eff51f8ba070a6ef90a
Santii05-C <santi.c.pgm@gmail.com>
2025-07-28 20:47:28 -0300

    Añadimos la función de sortear amigo y que pueda elejir al ganador.

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-function mostrarAmigos(amigos) {
-  // Obtener el elemento de la lista de amigos
-  const lista = document.getElementById("listaAmigos");
-
-  //  Limpiar la lista existente
-  lista.innerHTML = "";
-
-  // Iterar sobre el arreglo y 4. Agregar elementos a la lista
-  for (let i = 0; i < amigos.length; i++) {
-    lista.innerHTML += `<li>${amigos[i]}</li>`;
-  }
-}
-
-const misAmigos = ["Sergio", "Nicolas", "Agustina", "Estela"];
+const misAmigos = [""];
 mostrarAmigos(misAmigos);
 
+/*Función para agregar un amigo al array, verificar que no esté vacio y limpiar el input*/
 function agregarAmigo() {
-  //Captura el valor del campo de entrada
   const inputAmigo = document.getElementById("amigo");
   const nombreAmigo = inputAmigo.value.trim();
 
-  // Valida la entrada
   if (nombreAmigo === "") {
     alert("Por favor, inserte un nombre.");
     return;
   }
 
-  //Actualiza el array de amigos
   misAmigos.push(nombreAmigo);
 
-  //Limpiar el campo de entrada
   inputAmigo.value = "";
 
-  //Muestra los amigos actualizados
   mostrarAmigos(misAmigos);
+}
+
+/*Función para mostrar el amigo recien agregado y lo colocamos en una lista*/
+function mostrarAmigos(amigos) {
+  const lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+/*Función para sortear el un ganador de nuestra lista de amigos, verificar que la lista no esté vacía y mostramos el ganador */
+function sortearAmigo() {
+  const resultado = document.getElementById("resultado");
+  if (misAmigos.length === 0) {
+    alert("No hay amigos para sortear");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * misAmigos.length);
+  const nombreGanador = misAmigos[indiceAleatorio];
+  resultado.innerHTML = `El ganador es: ${misAmigos[indiceAleatorio]}`;
 }
