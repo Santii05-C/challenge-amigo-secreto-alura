ec2eaa173d0682082747a3a95dc7573d4c37efe8
Santii05-C <santi.c.pgm@gmail.com>
2025-07-28 11:03:11 -0300

    Añadimos la funcipon para mostrar el numbre ingrasado en el input.

--- app.js
+++ app.js
@@ -10,5 +10,27 @@ function mostrarAmigos(amigos) {
     lista.innerHTML += `<li>${amigos[i]}</li>`;
   }
 }
+
 const misAmigos = ["Sergio", "Nicolas", "Agustina", "Estela"];
 mostrarAmigos(misAmigos);
+
+function agregarAmigo() {
+  //Captura el valor del campo de entrada
+  const inputAmigo = document.getElementById("amigo");
+  const nombreAmigo = inputAmigo.value.trim();
+
+  // Valida la entrada
+  if (nombreAmigo === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  //Actualiza el array de amigos
+  misAmigos.push(nombreAmigo);
+
+  //Limpiar el campo de entrada
+  inputAmigo.value = "";
+
+  //Muestra los amigos actualizados
+  mostrarAmigos(misAmigos);
+}
